095b36e1472c4745a354131e40f5bb742af6a65f
Ian Saultz <52051793+atierian@users.noreply.github.com>
2021-11-18 17:57:03 -0500

    Remove empty tests

--- HostApp/HostAppUITests/HostAppUITests.swift
+++ HostApp/HostAppUITests/HostAppUITests.swift
@@ -7,24 +7,4 @@
 
 import XCTest
 
-class HostAppUITests: XCTestCase {
-
-    override func setUpWithError() throws {
-        continueAfterFailure = false
-    }
-
-    override func tearDownWithError() throws { }
-
-    func testExample() throws {
-        let app = XCUIApplication()
-        app.launch()
-    }
-
-    func testLaunchPerformance() throws {
-        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 7.0, *) {
-            measure(metrics: [XCTApplicationLaunchMetric()]) {
-                XCUIApplication().launch()
-            }
-        }
-    }
-}
+class HostAppUITests: XCTestCase { }
